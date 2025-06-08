d80ba5d5ca8ce289d1ab6ca09ae9a93eec33cbf0
MaxymBeregovoi <darlinggbm@gmail.com>
2025-06-08 21:07:56 +0300

    axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,18 @@
-function getImagesApi(q){
-  const searchParams = new URLSearchParams( {
-    key: "50733939-cc357c6b4bbe4fcbb86f08b26",
-    q: q,
-    image_type: "photo",
-    orientation: "horizontal",
-    safesearch: "true",
-  })
+import axios from 'axios';
 
-  return fetch(`https://pixabay.com/api/?${searchParams.toString()}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
+export function getImagesApi(q) {
+  return axios
+    .get('https://pixabay.com/api/', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      params: {
+        key: '50733939-cc357c6b4bbe4fcbb86f08b26',
+        q: q,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
     })
+    .then(response => response.data);
 }
-
-export {getImagesApi}
